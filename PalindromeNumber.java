87a39ad2daf508cef5cee2cf0bc0c1f9d1247a8e
beiniumeshwari27 <beiniumeshwari@gmail.com>
2025-09-12 09:43:38 +0530

    jfs41

---
+++ PalindromeNumber.java
@@ -0,0 +1,31 @@
+package com.codegnan.controlstatements;
+import java.util.Scanner;
+public class PalindromeNumber {
+
+	public static void main(String[] args) {
+		        Scanner scanner = new Scanner(System.in);
+		        System.out.print("Enter a number: ");
+		        int num = scanner.nextInt();
+		        scanner.close();
+
+		        int originalNum = num;
+		        int reversedNum = 0;
+
+		        while (num > 0) {
+		            int digit = num % 10;
+		            reversedNum = reversedNum * 10 + digit;
+		            num /= 10;
+		        }
+
+		        if (originalNum == reversedNum) {
+		            System.out.println(originalNum + " is a palindrome number");
+		        } else {
+		            System.out.println(originalNum + " is not a palindrome number");
+		        }
+		        scanner.close();
+		    }
+		
+
+	}
+
+
